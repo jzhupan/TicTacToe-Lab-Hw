a8e5c770a0d7467bd15b166b9e9b5f65fac813cb
Jennie Zhu <119384149+jzhupan@users.noreply.github.com>
2023-03-27 18:26:42 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//JS code goes here!
+
+//remember to Whiteboard and Pseduocode before diving into the actual code!
+
+
+//getting DOM elements
+
+
+//win conditions
+
+
+//game logic
+
+
+//what else will you need?
