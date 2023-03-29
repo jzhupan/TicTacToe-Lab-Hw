19232a05bfb123f77d731326ac0fc3ea40fe9582
jzhupan <jzhupan@gmail.com>
2023-03-29 18:13:05 -0400

    Attempted to determine winner for the tic-tac-toe

--- app.js
+++ app.js
@@ -26,15 +26,14 @@
 */
 
 
-let scoreBoard = document.querySelectorAll('.score-row')
+let scoreBoard = document.getElementsByClassName('score-row')
 let gameBoardCells = document.querySelectorAll('.square')
-let restartButton = document.getElementsByClassName('restart')
-
-
-//console.log(gameBoardCells)
-
+//getElementByClassName creates an array since classes are not unique unlike Ids, addEventListener 
+//does not work on arrays so you must specify the index if you are calling element by class name 
+let restartButton = document.getElementsByClassName('restart')[0]
+let isGameOver = false
 let player = 'x'
-let winningConditions = [
+let winningConditionsList = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -47,11 +46,9 @@ let winningConditions = [
     [2,4,6],
 ]
 
-
-
-// let emptyBoard = ["","","","","","","","","",]
-
-
+//console.log(restartButton)
+//console.log(gameBoardCells)
+//console.log(scoreBoard)
 
 
 let changePlayer = gameBoardCells.forEach(function(cell) {
@@ -60,6 +57,11 @@ let changePlayer = gameBoardCells.forEach(function(cell) {
             
             cell.innerHTML = player
             player == 'x' ? player = 'o' : player = 'x'
+
+            if(determineWinner()){
+                console.log(`${player} is the winner`)
+                isGameOver = true
+            }
         }
         
     }); 
@@ -67,39 +69,63 @@ let changePlayer = gameBoardCells.forEach(function(cell) {
 })
 
 
+restartButton.addEventListener('click', (gameBoardCells, clearCells)=> {
+    //console.log('reset button working')
+    gameBoardCells = document.querySelectorAll('.square')
+    //console.log(gameBoardCells)
+    for(let i = 0; i < gameBoardCells.length; i++){
+        clearCells = gameBoardCells[i].innerHTML = ''
+    }
+    
+})
 
 
-function gameOver() {
+//console.log(winningConditionsList)
 
+
+function determineWinner(){
+    let square0 = document.getElementById('square0')
+    let square1 = document.getElementById('square1')
+    let square2 = document.getElementById('square2')
+    let square3 = document.getElementById('square3')
+    let square4 = document.getElementById('square4')
+    let square5 = document.getElementById('square5')
+    let square6 = document.getElementById('square6')
+    let square7 = document.getElementById('square7')
+    let square8 = document.getElementById('square8')
+
+    if(square0.value === square1.value === square2.value){
+        console.log('game is over')
+        return true
+    } else if(square3.value === square4.value === square5.value){
+        console.log('game is over')
+        return true
+    } else if(square6.value === square7.value === square8.value){
+        console.log('game is over')
+        return true
+    } else if(square0.value === square3.value === square6.value){
+        console.log('game is over')
+        return true
+    } else if(square1.value === square4.value === square7.value){
+        console.log('game is over')
+        return true
+    } else if(square2.value === square5.value === square8.value){
+        console.log('game is over')
+        return true
+    } else if(square6.value === square7.value === square8.value){
+        console.log('game is over')
+        return true
+    } else if(square0.value === square4.value === square8.value){
+        console.log('game is over')
+        return true
+    }
 }
 
-function gameReset(){
-
-}
-
-function currentPlayersTurn(){
-
-}
-
-function gameScore(){
-
-}
-
-function detectWinner(){
-
-}
 
 
+// function gameScore(){
 
+// }
 
-
-
-// gameBoardCells.forEach(function gameCellsClick(cells, index){
-//     cells.addEventListener("click", () => {
-//         //console.log(`you clicked ${index}`)
-//         cells.innerHTML = `${index}`
-//     })
-
-// })
 
 
